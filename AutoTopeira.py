600aedb0154d82aa120498127d5d790acbd521af
David <dfl8@aluno.ifal.edu.br>
2022-08-12 11:50:07 -0300

    Update

--- AutoTopeira.py
+++ AutoTopeira.py
@@ -1,15 +1,15 @@
-from http.client import CONTINUE
 from time import sleep
 import pyautogui
 
-imglist = ["img\\Alquimistas.PNG", "img\\Benzeno.PNG", "img\\Carbono.PNG", "img\\Fogo.PNG", "img\\Joaquim.PNG", "img\\Kekule.PNG", "img\\Petroleo.PNG", "img\\Plásticos.PNG", "img\\Uréia.PNG", "img\\Wholer.PNG", "img\\Teste.PNG"]
+imglist = ["img\\Alquimistas.PNG", "img\\Benzeno.PNG", "img\\Carbono.PNG", "img\\Fogo.PNG", "img\\Joaquim.PNG", "img\\Kekule.PNG", "img\\Petroleo.PNG", "img\\Plásticos.PNG", "img\\Uréia.PNG", "img\\Wholer.PNG"]
 
-while True:
+execucao = True
+
+while execucao:
     for imagem in imglist:
-        try:
-            localimg = pyautogui.locateOnScreen(imagem)
+        localimg = pyautogui.locateOnScreen(imagem)
+        if localimg != None:
             pyautogui.click(localimg[0], localimg[1])
-            print(localimg)
-            print("passou")
-        except:
+            continue
+        else:
             continue
